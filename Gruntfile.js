c58dcf1c027440307b1260b97362b62ee6892af6
Patrick Roderman <patrickroderman@gmail.com>
2016-05-31 17:03:37 -0400

    changed spacing

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,96 +10,94 @@ module.exports = function(grunt) {
     grunt.initConfig({
         connect: { 
             server:{
-            options: {
-                port: 9000,
-                base: 'server/build/',
-                livereload: true,
-                open: {
-                target: "http://localhost:9000"
+                options: {
+                    port: 9000,
+                    base: 'server/build/',
+                    livereload: true,
+                    open: {
+                        target: 'http://localhost:9000'
+                    }
                 }
-            }
             }
         },
         watch: {
             scripts: {
-            files: ['server/build/**/*.js'],
-            options:{
-                reload: true
-            }
+                files: ['server/build/**/*.js'],
+                options:{
+                    reload: true
+                }
             }
         },
         browserify: {
             server: {
-            options: { 
-                watch: true,
-                browserifyOptions: {
-                standalone: 'adWidget'
-                }, 
-                 transform: [
-                    ["babelify",{ presets: ["react", "es2015"] }]
-                 ]
-            },
-            src: ['./src/index.js'],
-            dest:  './server/build/index.js',
-             
+                options: { 
+                    watch: true,
+                    browserifyOptions: {
+                        standalone: 'adWidget'
+                    }, 
+                    transform: [
+                    ['babelify',{ presets: ['react', 'es2015'] }]
+                    ]
+                },
+                src: ['./src/index.js'],
+                dest:  './server/build/index.js'             
             },
             module:{
-            options: {
-                watch: true,
-                browserifyOptions: {
-                standalone: 'adWidget'
-                }, 
-                 transform: [
-                    ["babelify",{ presets: ["react", "es2015"] }],
-                    ["envify", {
-                    global: true,
-                    NODE_ENV:"production",
-                    RC_ENV: grunt.option('rc-env') || 'production'
+                options: {
+                    watch: true,
+                    browserifyOptions: {
+                        standalone: 'adWidget'
+                    }, 
+                    transform: [
+                    ['babelify',{ presets: ['react', 'es2015'] }],
+                    ['envify', {
+                        global: true,
+                        NODE_ENV:'production',
+                        RC_ENV: grunt.option('rc-env') || 'production'
                     }]
-                 ]
-            },
-            src: ['./src/index.js'],
-            dest:  './dist/index.js',
-
+                    ]
+                },
+                src: ['./src/index.js'],
+                dest:  './dist/index.js'
             }
             
         },
         uglify: {
             my_target: {
-            files: {
-                'dist/index.min.js': ['dist/index.js']
-            }
+                files: {
+                    'dist/index.min.js': ['dist/index.js']
+                }
             }
         },
         compress: {
             options: {
-            mode: 'gzip',
-            level: 9
+                mode: 'gzip',
+                level: 9
             },
             build: {
-            files: [{
-            expand: true,
-            cwd: 'dist/',
-            src: ['index.min.js'],
-            dest: 'dist/'
-            }]
-            },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: ['index.min.js'],
+                    dest: 'dist/'
+                }]
+            }
         }
     });
     grunt.registerTask('s',  [
-    'browserify:server',
-    'connect:server',
-    'watch:scripts'
+        'browserify:server',
+        'connect:server',
+        'watch:scripts'
     ]
     );
     grunt.registerTask('build',[
-    'browserify:module',
-    'uglify',
-    'compress:build'
+        'browserify:module',
+        'uglify',
+        'compress:build'
     ]);
     grunt.registerTask('module',[
-    'browserify:module',
-    'uglify'
+        'browserify:module',
+        'uglify'
     ]);
 
 };
