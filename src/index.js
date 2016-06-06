34fe8bc5327a0ee41212c31c7d362beb759f8dde
Patrick Roderman <patrickroderman@gmail.com>
2016-06-06 16:35:58 -0400

    changed indentation

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import React from 'react';
 import Views from './Views';
 
 export function render(ele, {titleText = 'Promote your app', subtitleText = 'Search for your App on iTunes', buttonText = 'Preview', showLoadingAnimation = true}) {
-	ReactDOM.render(<Views options = {{titleText, subtitleText, buttonText, showLoadingAnimation}} ></Views>, ele);
+    ReactDOM.render(<Views options = {{titleText, subtitleText, buttonText, showLoadingAnimation}} ></Views>, ele);
 }
