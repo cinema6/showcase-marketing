36130faf04901b1ed1ef5f006491f9054d0f612c
Patrick Roderman <patrickroderman@gmail.com>
2016-06-09 09:31:46 -0400

    added interval param

--- src/components/AppAd.js
+++ src/components/AppAd.js
@@ -3,6 +3,7 @@ import React, { Component, PropTypes } from 'react';
 import { createInterstitialFactory } from 'showcase-core/dist/factories/app';
 import AdPreview from 'showcase/src/components/AdPreview';
 import { apiURL } from '../environment';
+import { assign } from 'lodash';
 
 const PREVIEW = {
     CARD_OPTIONS: {
@@ -21,13 +22,16 @@ export default class AppAd extends Component{
     render(){
         return(
         <AdPreview
-            cardOptions = {PREVIEW.CARD_OPTIONS}
+            cardOptions = { assign({}, 
+                                PREVIEW.CARD_OPTIONS, 
+                                {advanceInterval: this.props.interval}) }
             placementOptions = {PREVIEW.PLACEMENT_OPTIONS}
             productData = {this.props.appObj}
             factory = {createInterstitialFactory} 
             apiRoot = {apiURL}
             showLoadingAnimation={this.props.showLoadingAnimation}
             loadDelay= { this.props.showLoadingAnimation ? 3000 : 0 }
+
             />
         );
     }
@@ -35,5 +39,6 @@ export default class AppAd extends Component{
 
 AppAd.propTypes = {
     appObj: PropTypes.object.isRequired,
-    showLoadingAnimation: PropTypes.bool.isRequired
+    showLoadingAnimation: PropTypes.bool.isRequired,
+    interval: PropTypes.number.isRequired
 };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ export function render(ele, {
     titleText = 'Promote your app',
     subtitleText = 'Search for your App on iTunes', 
     buttonText = 'Preview', 
-    showLoadingAnimation = true}) {
+    showLoadingAnimation = true,
+	interval = 3
+	}) {
     ReactDOM.render(<Views 
-        options = {{titleText, subtitleText, buttonText, showLoadingAnimation}} >
+        options = {{titleText, subtitleText, buttonText, showLoadingAnimation, interval}} >
         </Views>, ele);
 }
